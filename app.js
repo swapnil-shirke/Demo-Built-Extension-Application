42a2bd65567b9c13d10cdabe38bcf4744a16c376
clivemchd <clivemchd@gmail.com>
2017-12-01 14:26:52 +0530

    redploying in extensions

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = Built.App('blt6eef1c94c2ce0f4f')
 .setMasterKey('blta53058e6ac1fc255')
 
 var extensionSDK = app.Extension({
-	secret_key     : 'blt471c38a9dabc682a',
+	secret_key     : 'blt81fdec8a4d1a2361',
 	extension_key	 : 'blt_ext_default',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')
