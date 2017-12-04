25f303467e5e746429d7fe6d4181f987037d2681
clivemchd <clivemchd@gmail.com>
2017-12-04 17:04:58 +0530

    - when installed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = Built.App('blt6eef1c94c2ce0f4f')
 .setMasterKey('blta53058e6ac1fc255')
 
 var extensionSDK = app.Extension({
-	secret_key     : 'blt9375a25981120eb5',
+	secret_key     : 'blte0c2f5b51a07e5f3',
 	extension_key	 : 'blt_ext_default',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')
