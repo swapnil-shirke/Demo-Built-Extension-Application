e81e1f6a863c77c2aa0b72048bba07341506f816
clivemchd <clivemchd@gmail.com>
2017-12-04 13:56:26 +0530

    adds hooks when module as dependency

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,28 @@
-// var when = require("when");
+var when = require("when");
 
 module.exports = {
   "/v1/functions/clive": {
-		GET: function(req, res){
+		GET: function(req, res) {
 			this.resSuccess(req, res, {
 				clive: "is awesome"
 			})
+		}
+	},
+	"/v1/classes/person/objects" : {
+		POST: {
+			_pre: function(req, res) {
+				console.log("Pre", req, " ---- ", res);
+				req.bobjekt = req.bobjekt.set("age", 44)
+				req.bobjekt = req.bobjekt.setReferenceWhere("address", {
+					"city": "Mumbai"
+				})
+				return when.resolve()
+			},
+			_post: function(req, res) {
+				console.log("Post", req, " ---- ", res);				
+				req.bobjekt['new_field'] = "new_value"
+				return when.resolve()
+			}
 		}
 	}
 }	
