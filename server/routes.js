367407b405cb04e085d149d4979010fa9b8765c9
clivemchd <clivemchd@gmail.com>
2017-12-04 16:13:32 +0530

    - install when - updates secert key

--- server/routes.js
+++ server/routes.js
@@ -8,21 +8,21 @@ module.exports = {
 			})
 		}
 	},
-	// "/v1/classes/person/objects" : {
-	// 	POST: {
-	// 		_pre: function(req, res) {
-	// 			req.logger.log("Pre", req, " ---- ", res)
-	// 			req.bobjekt = req.bobjekt.set("age", 44)
-	// 			// req.bobjekt = req.bobjekt.setReferenceWhere("address", {
-	// 			// 	"city": "Mumbai"
-	// 			// })
-	// 			return when.resolve()
-	// 		},
-	// 		_post: function(req, res) {
-	// 			req.logger.log("Post", req, " ---- ", res)			
-	// 			req.bobjekt['new_field'] = "new_value"
-	// 			return when.resolve()
-	// 		}
-	// 	}
-	// }
+	"/v1/classes/person/objects" : {
+		POST: {
+			_pre: function(req, res) {
+				req.logger.log(res)
+				req.bobjekt = req.bobjekt.set("age", 44)
+				// req.bobjekt = req.bobjekt.setReferenceWhere("address", {
+				// 	"city": "Mumbai"
+				// })
+				return when.resolve()
+			},
+			_post: function(req, res) {
+				req.logger.log(res)			
+				req.bobjekt['new_field'] = "new_value"
+				return when.resolve()
+			}
+		}
+	}
 }	
