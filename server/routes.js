837c7634e93f2de57594fd0269f7459d838ef784
clivemchd <clivemchd@gmail.com>
2017-12-04 15:34:35 +0530

    - adds age 44 - removes set reference

--- server/routes.js
+++ server/routes.js
@@ -11,15 +11,15 @@ module.exports = {
 	"/v1/classes/person/objects" : {
 		POST: {
 			_pre: function(req, res) {
-				console.log("Pre", req, " ---- ", res);
+				req.logger.log("Pre", req, " ---- ", res)
 				req.bobjekt = req.bobjekt.set("age", 44)
-				req.bobjekt = req.bobjekt.setReferenceWhere("address", {
-					"city": "Mumbai"
-				})
+				// req.bobjekt = req.bobjekt.setReferenceWhere("address", {
+				// 	"city": "Mumbai"
+				// })
 				return when.resolve()
 			},
 			_post: function(req, res) {
-				console.log("Post", req, " ---- ", res);				
+				req.logger.log("Post", req, " ---- ", res)			
 				req.bobjekt['new_field'] = "new_value"
 				return when.resolve()
 			}
