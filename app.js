1064e979dca84e5ff06f0a5745d6bad415300d3d
clivemchd <clivemchd@gmail.com>
2017-12-04 10:09:14 +0530

    - Updates secret key in app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = Built.App('blt6eef1c94c2ce0f4f')
 .setMasterKey('blta53058e6ac1fc255')
 
 var extensionSDK = app.Extension({
-	secret_key     : 'blt2989e11376f9fd6a',
+	secret_key     : 'blt792d44b599edfc5b',
 	extension_key	 : 'blt_ext_default',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')
