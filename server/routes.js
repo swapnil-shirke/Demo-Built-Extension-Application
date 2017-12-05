9a2002b0459862982d2dcccea8c6ebfa581706b0
Clive Machado <clive.machado@raweng.com>
2017-12-05 17:21:43 +0530

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -12,7 +12,7 @@ module.exports = {
 		POST: {
 			_pre: function(req, res) {
 				req.logger.log("pre")
-				req.bobjekt = req.bobjekt.set("age", 89)
+				req.bobjekt = req.bobjekt.set("age", 40)
 				req.bobjekt.set('address', "Mumbai")
 				
 				// req.bobjekt = req.bobjekt.setReferenceWhere("address", {
