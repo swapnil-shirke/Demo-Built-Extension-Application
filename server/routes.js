425d0ae6c813755378a4aef8b4de4c9c5bcd2444
clivemchd <clivemchd@gmail.com>
2017-12-07 12:59:25 +0530

    12:59

--- server/routes.js
+++ server/routes.js
@@ -38,6 +38,31 @@ module.exports = {
 				req.logger.warn(req.bobjekt)
 				return when.resolve()				
 			}
+		},
+		PUT: {
+			_pre: function(req, res) {
+				req.bobjekt = req.bobjekt.set("name", "Rohini")
+				req.logger.log("pre updated:  ", req.bobjekt)								
+				return when.resolve()
+			},
+			_post: function(req, res) {
+				req.logger.log("post updated:  ", req.bobjekt)												
+				return when.resolve()				
+			}
+		},
+		DELETE: {
+			_pre: function(req, res) {	
+				req.logger.log("Deleted:  ", req.bobjekt)				
+				return when.resolve()
+			},
+			_post: function(req, res) {
+				var defered = utils.Promise.defer()				
+				setTimeout(function(){
+					req.logger.warn("Timeout completed")
+					defered.resolve()
+				},1000)
+				return when.resolve()				
+			}
 		}
 	}
 }	
