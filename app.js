5cc0b5df0ad07e85250ad683da6db7c3380f0dc0
swapnil-shirke <swapnil.shirke@raweng.com>
2018-03-05 18:28:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('bltc8b6b9e952929343')
+var app = Built.App('bltca4688e817a165b4')
 .setHost("stag-api.built.io")
 .setProtocol("https")
-.setMasterKey('blt13a73f2b5ae3a00e')
+.setMasterKey('blt6d76ddfb84a8cb60')
 
 var extensionSDK = app.Extension({
 	secret_key     : 'swapnil23',
